0d2ded626de2652ee6c73290298630f5c2a31851
Iambian <iambian@gmail.com>
2020-05-17 20:30:08 -0400

    Ensures build directories exist

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 del /Q obj\* >nul 2>&1
+if not exist "bin\" mkdir bin\
+if not exist "obj\" mkdir obj\
 tools\spasm64 -E src\game.asm obj\game.bin
 tools\zx7 obj\game.bin obj\game.zx7
 tools\spasm64 -E src\main.asm bin\FALLDOWN.8xp
